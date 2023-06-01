fc926cc8d6d8fc99cbde0e887d6ddcc19b5fc153
Sidnei Farias <sidneijr.77@gmail.com>
2023-06-01 01:41:24 -0300

    Realiza o cálculo das transações, em Summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,7 +6,25 @@ import { TransactionsContext } from "../../contexts/TransactionsContext";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
-  console.log(transactions)
+  // reduce() permite percorrer um array e reduzir o mesmo a uma nova estrutura de dados.
+  // Ou seja: quero reduzir meu transactions, à essa estrutura de dados:
+  // { income: 0, outcome: 0, total: 0 }
+
+  // acc -> accumulator. Refere-se ao objeto { income: 0, outcome: 0, total: 0 }.
+  // Todas as operações no reducer, farei no accumulator.
+  const summary = transactions.reduce((acc, transaction) => {
+    if(transaction.type === 'income'){
+      acc.income += transaction.price;
+      acc.total += transaction.price;
+    }
+
+    else {
+      acc.outcome += transaction.price;
+      acc.total -= transaction.price;
+    }
+
+    return acc;
+  }, { income: 0, outcome: 0, total: 0 });
 
   return (
     <SummaryContainer>
@@ -16,7 +34,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -25,7 +43,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -34,7 +52,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#ffffff" />
         </header>
 
-        <strong>R$17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
