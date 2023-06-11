6e42cb5d4dbb8f7151b402377b4efe3ecc6e418e
Sidnei Farias <sidneijr.77@gmail.com>
2023-06-11 17:26:37 -0300

    Inclui um modal acessível, com Radix UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionbutton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import logoImg from '../../assets/logo.svg'
 
@@ -8,7 +9,29 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionbutton>Nova transação</NewTransactionbutton>
+        <Dialog.Root>
+          {/**
+           * O atributo 'asChild' irá impedir que um novo botão seja criado.
+           * Ao invés disso, aproveite o botão que já existe dentro da tag (no caso, NewTransactionButton).
+           */}
+          <Dialog.Trigger asChild>
+            <NewTransactionbutton>Nova transação</NewTransactionbutton>
+          </Dialog.Trigger>
+
+          {/**
+           * O modal não necessariamente faz parte do Header; apenas o botão atrelado à abertura do mesmo está presente no Header.
+           * Assim, o uso de Portal é justificado, pois o Modal é um overlay sobreposto à página inteira e fora do contexto do Header.
+           */}
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
