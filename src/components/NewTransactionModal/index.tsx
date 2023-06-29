25a43e0f5dbd6576878036c0e42677345bd303ef
Sidnei Farias <sidneijr.77@gmail.com>
2023-06-28 23:55:49 -0300

    Implementa Context Selectors

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,8 +5,8 @@ import * as z from 'zod'
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
-import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { useContextSelector } from "use-context-selector";
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
@@ -18,7 +18,9 @@ const newTransactionFormSchema = z.object({
 type newTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
-  const { createTransaction } = useContext(TransactionsContext)
+  const createTransaction = useContextSelector(TransactionsContext, (context) => {
+    return context.createTransaction;
+  })
 
   const {
     control,
--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,8 +1,10 @@
-import { useContext } from "react";
 import { TransactionsContext } from "../contexts/TransactionsContext";
+import { useContextSelector } from "use-context-selector";
 
 export function useSummary() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, context => {
+    return context.transactions
+  })
 
   /**
    * reduce() permite percorrer um array e reduzir o mesmo a uma nova estrutura de dados.
